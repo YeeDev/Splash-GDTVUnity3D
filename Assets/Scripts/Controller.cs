41039c794ba0558aa1df89aff20213a6ba7cea86
Yee <yee.gamedev@gmail.com>
2023-05-05 13:33:11 -0600

    Mover Refactored - Added Controller Script

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Mover))]
+public class Controller : MonoBehaviour
+{
+    [SerializeField] Transform groundChecker;
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] float checkerRadius;
+
+    float hAxis = 0;
+    float vAxis = 0;
+    Mover mover;
+
+    private bool IsGrounded => Physics.CheckSphere(groundChecker.position, checkerRadius, groundMask);
+
+    private void Awake()
+    {
+        mover = GetComponent<Mover>();
+    }
+
+    private void Update()
+    {
+        SetMoveAxis();
+        JumpInput();
+        TurnInput();
+    }
+
+    void FixedUpdate() => mover.Move(vAxis, hAxis);
+
+    private void SetMoveAxis()
+    {
+        hAxis = Input.GetAxis("Horizontal");
+        vAxis = Input.GetAxis("Vertical");
+    }
+
+    private void JumpInput()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded) { mover.Jump(vAxis, hAxis); }
+        else if (Input.GetButtonUp("Jump") && !IsGrounded) { mover.HaltJump(); }
+    }
+
+    private void TurnInput() => mover.Turn(hAxis);
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (groundChecker == null) { return; }
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(groundChecker.position, checkerRadius);
+    }
+#endif
+}
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,46 +3,55 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float forwardSpeed = 5;
-    [SerializeField] float backWalkFactor = 2;
-    [SerializeField] float turnSpeed = 5;
-    [SerializeField] float jumpForce = 10;
-    [SerializeField] float turnSpeedPenalty = 0.1f;
-    [SerializeField] Transform groundChecker;
-    [SerializeField] LayerMask groundMask;
-    [SerializeField] float checkerRadius;
+    [Range(0, 10)][SerializeField] float forwardSpeed = 5;
+    [Range(0, 10)][SerializeField] float backWalkFactor = 2;
+    [Range(0, 10)][SerializeField] float turnSpeedPenalty = 0.1f;
+    [Range(0, 10)][SerializeField] float turnSpeed = 5;
+    [Range(0, 10)][SerializeField] float jumpForce = 10;
 
     Rigidbody rb;
 
     private void Awake() => rb = GetComponent<Rigidbody>();
 
-    private void Update()
+    //Called in Controller in - JumpInput
+    public void Jump(float vAxis, float hAxis)
     {
-        if (Input.GetButtonDown("Jump") && Physics.CheckSphere(groundChecker.position, checkerRadius, groundMask))
-        {
-            Vector3 jumpSpeed = rb.velocity;
-            jumpSpeed.y = jumpForce * Input.GetAxis("Vertical");
-            rb.velocity = jumpSpeed;
-        }
-        else if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
-        {
-            Vector3 haltSpeed = rb.velocity;
-            haltSpeed.y *= 0.5f;
-            rb.velocity = haltSpeed;
-        }
+        Vector3 jumpSpeed = rb.velocity;
+        jumpSpeed.y = jumpForce * (CalculateWalkSpeed(vAxis, hAxis) / forwardSpeed);
+        rb.velocity = jumpSpeed;
     }
 
-    void FixedUpdate()
+    //Called in Controller in - JumpInput
+    public void HaltJump()
     {
-        float walkSpeed = Input.GetAxis("Vertical");
-        walkSpeed *= walkSpeed < 0 ? backWalkFactor : forwardSpeed;
-        walkSpeed = Mathf.Clamp(walkSpeed - Mathf.Abs(Input.GetAxis("Horizontal")) * turnSpeedPenalty, 0 , walkSpeed);
-        Vector3 translation = transform.forward * walkSpeed;
-        rb.MovePosition(transform.position + translation * Time.fixedDeltaTime);
+        if (rb.velocity.y <= 0) { return; }
 
-        if (Input.GetButton("Horizontal"))
+        Vector3 jumpSpeed = rb.velocity;
+        jumpSpeed.y *= 0.5f;
+        rb.velocity = jumpSpeed;
+    }
+
+    //Called in Controller in - FixedUpdate
+    public void Move(float vAxis, float xAxis)
+    {
+        Vector3 translation = transform.forward * CalculateWalkSpeed(vAxis, xAxis);
+        rb.MovePosition(transform.position + translation * Time.fixedDeltaTime);
+    }
+
+    //Called in Controller in - TurnInput
+    public void Turn(float hAxis) => transform.Rotate(transform.up * turnSpeed * hAxis);
+
+    private float CalculateWalkSpeed(float vAxis, float hAxis)
+    {
+        float walkSpeed = vAxis;
+        walkSpeed *= walkSpeed < 0 ? backWalkFactor : forwardSpeed;
+
+        if (vAxis > Mathf.Epsilon)
         {
-            transform.Rotate(transform.up * turnSpeed * Input.GetAxisRaw("Horizontal"));
+            walkSpeed = Mathf.Clamp(walkSpeed - Mathf.Abs(hAxis) * turnSpeedPenalty, 0, walkSpeed);
         }
+
+        Debug.Log(walkSpeed);
+        return walkSpeed;
     }
 }
