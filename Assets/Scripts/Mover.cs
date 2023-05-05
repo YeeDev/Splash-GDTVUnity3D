b32b9f9a43e567f1efc51333834b98604ea48ac5
Yee <yee.gamedev@gmail.com>
2023-05-05 11:46:15 -0600

    Player Can Move

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float speed = 5;
+    [SerializeField] float turnSpeed = 5;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 translation = transform.forward * Input.GetAxis("Vertical") * speed;
+        rb.MovePosition(transform.position + translation * Time.fixedDeltaTime);
+
+        if (Input.GetButton("Horizontal"))
+        {
+            transform.Rotate(transform.up * turnSpeed * Input.GetAxisRaw("Horizontal"));
+        }
+    }
+}
