771411b22302c202b281d51917dabcb325f73ba9
Yee <yee.gamedev@gmail.com>
2023-05-05 13:58:57 -0600

    Spinner Obstacle created

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 0;
+
+    void Update()
+    {
+        Vector3 rotate = Vector3.up * rotationSpeed * Time.deltaTime;
+        transform.Rotate(rotate);
+    }
+
+}
