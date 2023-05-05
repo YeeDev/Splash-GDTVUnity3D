0bd34556bd4f9a29dc3442873b98369b5245d689
Yee <yee.gamedev@gmail.com>
2023-05-05 12:05:49 -0600

    Camera follows player

---
+++ Assets/Scripts/Tracker.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Splash.Core
+{
+    public class Tracker : MonoBehaviour
+    {
+        Transform player;
+
+        private void Awake() => player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        private void LateUpdate()
+        {
+            Vector3 trackPosition = player.position;
+            trackPosition.y = transform.position.y;
+            transform.position = trackPosition;
+        }
+    }
+}
